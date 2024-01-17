5a1eac874f9008fbd237da2f8ea3246d37396059
Dimitri <dimitri.lereverend@inria.fr>
2024-01-17 11:18:36 +0100

    Added plots for linkability attacks

--- results/utils.py
+++ results/utils.py
@@ -1,6 +1,8 @@
 import os
 
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 ATTRIBUTE_DICT = {
     "network_size": ["128nodes"],
@@ -92,6 +94,7 @@ def plot_evolution_iterations(
     current_attributes=None,
     attribute_mapping=None,
 ):
+    print(data.columns)
     data_to_consider = data[column_name].dropna()
     if current_attributes is None or attribute_mapping is None:
         plt.plot(data_to_consider.index, data_to_consider, label=name)
@@ -227,13 +230,20 @@ def scatter_aggregated_data(
     x_axis_name="iteration",
     current_attributes=None,
     attribute_mapping=None,
+    method="mean",
 ):
     if x_axis_name == "iteration":
         raise ValueError("Shouldn't scatter averaged data on iterations")
     else:
         data_to_consider = data[[x_axis_name, column_name]].dropna()
-        x_value = data_to_consider[x_axis_name].mean()
-        y_value = data_to_consider[column_name].mean()
+        if method == "mean":
+            x_value = data_to_consider[x_axis_name].mean()
+            y_value = data_to_consider[column_name].mean()
+        elif method == "max":
+            x_value = data_to_consider[x_axis_name].max()
+            y_value = data_to_consider[column_name].max()
+        else:
+            raise ValueError(f"Expected method in ['mean', 'max'], got {method}")
     if current_attributes is None or attribute_mapping is None:
         plt.scatter(x_value, y_value, label=name)
         return
@@ -262,6 +272,7 @@ def scatter_averaged_experiments(
     x_axis_name="iteration",
     figsize=(25, 25),
     save_directory=None,
+    method="mean",
 ):
     plt.figure(figsize=figsize)
     for experiment in sorted(experiments):
@@ -272,17 +283,59 @@ def scatter_averaged_experiments(
             x_axis_name=x_axis_name,
             current_attributes=experiments_attributes[experiment],
             attribute_mapping=display_attributes,
+            method=method,
         )
 
     plt.legend()
-    plt.ylabel(column_name + " mean")
-    plt.xlabel(x_axis_name + " mean")
+    plt.ylabel(f"{column_name} {method}")
+    plt.xlabel(f"{x_axis_name} {method}")
     plt.title(plot_name)
     if save_directory is not None:
         savefile = f"{save_directory}{plot_name.replace(' ','_')}.pdf"
         print(f"Saving to {savefile}")
         plt.savefig(savefile)
     return
+
+
+def fix_linkability_attack_results(experiment_name, attack_results_path):
+    expected_file_name = f"linkability_{experiment_name}.csv"
+    directories = sorted(os.listdir(attack_results_path))
+    if expected_file_name not in directories:
+        print(
+            f"Not loading attack results: {expected_file_name} was not listed with attack results in {attack_results_path}. Entire directory:\n{directories}"
+        )
+        raise FileNotFoundError(expected_file_name)
+
+    linkability_attack_df = pd.read_csv(
+        os.path.join(attack_results_path, expected_file_name)
+    )
+
+    # Fixing all the missing values/wrongly filled values
+    linkability_attack_df["linkability_top1"] = (
+        linkability_attack_df["linkability_top1_guess"]
+        == linkability_attack_df["agent"]
+    )
+
+    linkability_attack_df.reset_index(drop=True)
+    linkability_attack_df.set_index(["agent", "iteration"])
+    columns = linkability_attack_df.columns.to_list()
+    columns_losses = [column for column in columns if "loss_trainset_" in column]
+
+    linkability_attack_df["linkability_real_rank"] = np.nan
+    linkability_attack_df["linkability_real_rank"].astype("Int64", copy=False)
+    for index, row in linkability_attack_df.iterrows():
+        losses = [(int(column.split("_")[2]), row[column]) for column in columns_losses]
+        losses_sorted = sorted(losses, key=lambda x: x[1])
+        agents_sorted = [x[0] for x in losses_sorted]
+        linkability_rank = agents_sorted.index(row["agent"])
+        linkability_attack_df.at[index, "linkability_real_rank"] = linkability_rank
+
+    linkability_attack_df = linkability_attack_df.drop(columns="Unnamed: 0")
+    linkability_attack_df.to_csv(
+        os.path.join(attack_results_path, f"fixed_{expected_file_name}")
+    )
+
+    return linkability_attack_df
 
 
 if __name__ == "__main__":

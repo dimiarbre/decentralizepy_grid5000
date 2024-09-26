ed7ec7ce512d0b08ffe7643f512c5a56fb151af1
Dimitri Lerévérend <dimitri.lereverend@inria.fr>
2024-09-26 11:29:22 +0200

    Added tqdm to server config for classifier attack.

--- setup_control.sh
+++ setup_control.sh
@@ -3,5 +3,5 @@ module load cuda;
 python -m venv /tmp/custom_python; 
 source /tmp/custom_python/bin/activate; 
 mkdir /tmp/cache_python
-pip install --cache-dir /tmp/cache_python enoslib --editable decentralizepy/; 
+pip install --cache-dir /tmp/cache_python enoslib tqdm --editable decentralizepy/; 
 mkdir /tmp/logs

96be5c850508cc27aaefd3677349ab60008ef375
Dimitri Lerévérend <dimitri.lereverend@inria.fr>
2024-10-03 10:59:23 +0200

    Created script to submit jobs on oar.

---
+++ script_attacker_g5k.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+#OAR -n attacker
+#OAR -q production 
+#OAR -l gpu=1,walltime=36:00:00
+#OAR -O OAR_%jobid%.out
+#OAR -E OAR_%jobid%.err
+
+cd ~/scratch/decentralizepy_grid5000
+chmod +x setup_control.sh
+./setup_control.sh
+source /tmp/custom_python/bin/activate;
+
+python results/perform_attacks.py $@

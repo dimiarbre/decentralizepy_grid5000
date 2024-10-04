db970fa821c78ef743272f19bf06bac7d5d0f2aa
Dimitri Lerévérend <dimitri.lereverend@inria.fr>
2024-10-04 11:03:50 +0200

    Adapted configurations of scripts to hean-zay

--- synchronize_remote.sh
+++ synchronize_remote.sh
@@ -1,1 +1,27 @@
-rsync -avzP --exclude \\.* --include \*/decentralizepy/datasets/ --exclude datasets/  --exclude results/my_results --exclude results/assets/ --exclude results/datasets --exclude *.sif --exclude venv* --exclude \*/tmp* --exclude **__pycache__  ../decentralizepy_grid5000 $1.g5k:scratch/
+set -x
+remote=$1
+if [[ "$remote" == "jean-zay" ]]; then
+    rsync -avzP --info=progress2 \
+    --exclude '\\.*' --exclude datasets\
+    --exclude attacks/my_results\
+    --exclude attacks/assets\
+    --exclude attacks/datasets\
+    --exclude 'venv*'\
+    --exclude '**/.mypy_cache'\
+    --exclude '**/.git'\
+    --exclude '\*/tmp*' --exclude '**__pycache__'\
+    ../decentralizepy_grid5000 $remote:work/
+else
+    rsync -avzP --info=progress2 \
+    --include '\*/decentralizepy/datasets/'\
+    --exclude '\\.*' \
+    --exclude 'datasets/' \
+    --exclude 'attacks/my_results' \
+    --exclude 'attacks/assets/' \
+    --exclude 'attacks/datasets' \
+    --exclude '*.sif' \
+    --exclude 'venv*'\
+    --exclude '\*/tmp*' \
+    --exclude '**__pycache__'\
+    ../decentralizepy_grid5000 $remote.g5k:scratch/
+fi
